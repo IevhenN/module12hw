c6e547dc589733c224cf263af9208c7e109fab23
Ievhen <ievhennesterenko@gmail.com>
2024-02-11 21:13:05 +0200

    сorrection

--- src/main/java/task2/FizzBuzz.java
+++ src/main/java/task2/FizzBuzz.java
@@ -1,90 +1,58 @@
 package task2;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+class FizzBuzz {
+    private final int maxNumber;
+    private int currentNumber = 1;
 
-public class FizzBuzz {
-    private static final int FIZZ_BUZZ = 15;
-    private static final int FIZZ = 3;
-    private static final int BUZZ = 5;
-    private int currentNumeric;
-
-    private Queue<String> result = new LinkedList<>();
-
-    private String comma = "";
-    private boolean isFizzBuzz = true;
-    private boolean isFizz = true;
-    private boolean isBuzz = true;
-
-    public boolean processEnd() {
-        return isFizzBuzz && isBuzz && isFizz && result.isEmpty();
+    public FizzBuzz(int maxNumber) {
+        this.maxNumber = maxNumber;
     }
 
-    public boolean processNextNumeric(int nextNumeric) {
-        if (isFizzBuzz && isFizz && isBuzz) {
-            synchronized (this) {
-                currentNumeric = nextNumeric;
-                isFizzBuzz = false;
-                isFizz = false;
-                isBuzz = false;
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void fizzBuzz() {
-        if (!isFizzBuzz && !isFizz && !isBuzz) {
-            synchronized (this) {
-                if (currentNumeric % FIZZ_BUZZ == 0) {
-                    result.add("fizzbuzz");
-                    isFizzBuzz = true;
-                    isFizz = true;
-                    isBuzz = true;
-                } else {
-                    isFizzBuzz = true;
-                }
+    public synchronized void fizzbuzz() throws InterruptedException {
+        while (currentNumber <= maxNumber) {
+            if (currentNumber % 15 == 0) {
+                System.out.println("fizzbuzz");
+                currentNumber++;
+                notifyAll();
+            } else {
+                wait();
             }
         }
     }
 
-    public void fizz() {
-        if (isFizzBuzz && !isFizz) {
-            synchronized (this) {
-                if (currentNumeric % FIZZ == 0) {
-                    result.add("fizz");
-                    isFizzBuzz = true;
-                    isFizz = true;
-                    isBuzz = true;
-                } else {
-                    isFizz = true;
-                }
-            }
-
-        }
-    }
-
-    public void buzz() {
-        if (isFizz && !isBuzz) {
-            synchronized (this) {
-                if (currentNumeric % BUZZ == 0) {
-                    result.add("buzz");
-                } else {
-                    result.add(String.valueOf(currentNumeric));
-                }
-                isFizzBuzz = true;
-                isFizz = true;
-                isBuzz = true;
+    public synchronized void fizz() throws InterruptedException {
+        while (currentNumber <= maxNumber) {
+            if (currentNumber % 3 == 0 && currentNumber % 5 != 0) {
+                System.out.println("fizz");
+                currentNumber++;
+                notifyAll();
+            } else {
+                wait();
             }
         }
     }
 
-    public void number() {
-        if (!result.isEmpty()) {
-            System.out.print(comma + result.remove());
-            comma = ", ";
+    public synchronized void buzz() throws InterruptedException {
+        while (currentNumber <= maxNumber) {
+            if (currentNumber % 3 != 0 && currentNumber % 5 == 0) {
+                System.out.println("buzz");
+                currentNumber++;
+                notifyAll();
+            } else {
+                wait();
+            }
+        }
+    }
+
+    public synchronized void number() throws InterruptedException {
+        while (currentNumber <= maxNumber) {
+            if (currentNumber % 3 != 0 && currentNumber % 5 != 0) {
+                System.out.println(currentNumber);
+                currentNumber++;
+                notifyAll();
+            } else {
+                wait();
+            }
         }
     }
 }
--- src/main/java/task2/FizzBuzzTest.java
+++ src/main/java/task2/FizzBuzzTest.java
@@ -2,47 +2,35 @@ package task2;
 
 public class FizzBuzzTest {
     public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int n = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz(30);
 
         Thread threadA = new Thread(() -> {
-            while (true) {
+            try {
                 fizzBuzz.fizz();
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
+
         Thread threadB = new Thread(() -> {
-            while (true) {
+            try {
                 fizzBuzz.buzz();
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
         Thread threadC = new Thread(() -> {
-            while (true) {
-                fizzBuzz.fizzBuzz();
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            try {
+                fizzBuzz.fizzbuzz();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
         Thread threadD = new Thread(() -> {
-            while (true) {
+            try {
                 fizzBuzz.number();
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
 
@@ -50,24 +38,5 @@ public class FizzBuzzTest {
         threadB.start();
         threadC.start();
         threadD.start();
-
-        for (int i = 1; i <= n; i++) {
-            while (!fizzBuzz.processNextNumeric(i)) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        while (!fizzBuzz.processEnd()) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.exit(0);
     }
 }
